207189d6c3fe19566d4a3bfad062a69d554764dd
Alex Vukadinov <alexvuka1@gmail.com>
2024-02-10 20:36:04 +0000

    added inputs for docPath and oasPath

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import * as core from '@actions/core';
+import { readFile } from 'fs/promises';
 
 export const run = async (): Promise<void> => {
   try {
-    const str = core.getInput('str', { required: true });
-    core.debug(`Hello ${str}`);
+    const oasPath = core.getInput('openapi-path', { required: true });
+    const docPath = core.getInput('doc-path', { required: true });
+
+    const oas = await readFile(oasPath, 'utf8');
+    core.debug(oas);
+
+    const doc = await readFile(docPath, 'utf8');
+    core.debug(doc);
+
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     // Fail the workflow run if an error occurs
